c6e36ef043ad161bcad7f297546607e3e8afc0f6
Aaron Castro <aaron.castro1990@gmail.com>
2024-09-20 09:10:27 -0600

    new version 1.1.21 (#3)

--- lib/merb-core/gem_ext/erubis.rb
+++ lib/merb-core/gem_ext/erubis.rb
@@ -10,24 +10,24 @@ module Erubis
       pat = @pattern
       regexp = pat.nil? || pat == '<% %>' ? DEFAULT_REGEXP : pattern_regexp(pat)
       pos = 0
-      is_bol = true     # is beginning of line
+      is_bol = true # is beginning of line
       input.scan(regexp) do |indicator, code, tailch, rspace|
-        match = Regexp.last_match()
+        match = Regexp.last_match
         len  = match.begin(0) - pos
         text = input[pos, len]
         pos  = match.end(0)
         ch   = indicator ? indicator[0] : nil
-        lspace = ch == ?= ? nil : detect_spaces_at_bol(text, is_bol)
+        lspace = ch == '=' ? nil : detect_spaces_at_bol(text, is_bol)
         is_bol = rspace ? true : false
         add_text(src, text) if text && !text.empty?
         ## * when '<%= %>', do nothing
         ## * when '<% %>' or '<%# %>', delete spaces iff only spaces are around '<% %>'
-        if ch == ?=              # <%= %>
+        if ch == '='              # <%= %>
           rspace = nil if tailch && !tailch.empty?
           add_text(src, lspace) if lspace
           add_expr(src, code, indicator)
           add_text(src, rspace) if rspace
-        elsif ch == ?\#          # <%# %>
+        elsif ch == "\#"          # <%# %>
           n = code.count("\n") + (rspace ? 1 : 0)
           if @trim && lspace && rspace
             add_stmt(src, "\n" * n)
@@ -36,34 +36,31 @@ module Erubis
             add_stmt(src, "\n" * n)
             add_text(src, rspace) if rspace
           end
-        elsif ch == ?%           # <%% %>
-          s = "#{lspace}#{@prefix||='<%'}#{code}#{tailch}#{@postfix||='%>'}#{rspace}"
+        elsif ch == '%'           # <%% %>
+          s = "#{lspace}#{@prefix ||= '<%'}#{code}#{tailch}#{@postfix ||= '%>'}#{rspace}"
           add_text(src, s)
-        else                     # <% %>
-          if @trim && lspace && rspace
-            if respond_to?(:add_stmt2)
-              add_stmt2(src, "#{lspace}#{code}#{rspace}", tailch)
-            else
-              add_stmt(src, "#{lspace}#{code}#{rspace}")
-            end
+        elsif @trim && lspace && rspace # <% %>
+          if respond_to?(:add_stmt2)
+            add_stmt2(src, "#{lspace}#{code}#{rspace}", tailch)
           else
-            add_text(src, lspace) if lspace
-            if respond_to?(:add_stmt2)
-              add_stmt2(src, code, tailch)
-            else
-              add_stmt(src, code)
-            end
-            add_text(src, rspace) if rspace
+            add_stmt(src, "#{lspace}#{code}#{rspace}")
           end
+        else
+          add_text(src, lspace) if lspace
+          if respond_to?(:add_stmt2)
+            add_stmt2(src, code, tailch)
+          else
+            add_stmt(src, code)
+          end
+          add_text(src, rspace) if rspace
         end
       end
-      #rest = $' || input                        # ruby1.8
-      rest = pos == 0 ? input : input[pos..-1]   # ruby1.9
+      # rest = $' || input                        # ruby1.8
+      rest = pos == 0 ? input : input[pos..-1] # ruby1.9
       add_text(src, rest)
     end
-    
   end
-  
+
   class MEruby < Erubis::Eruby
     include PercentLineEnhancer
     include StringBufferEnhancer
@@ -78,7 +75,7 @@ module Erubis
   #   binding.
   #
   # :api: private
-  def self.load_yaml_file(file, binding = binding)
-    YAML::load(Erubis::MEruby.new(IO.read(File.expand_path(file))).result(binding))
+  def self.load_yaml_file(file, binding = nil)
+    YAML.load(Erubis::MEruby.new(IO.read(File.expand_path(file))).result(binding))
   end
 end
